94c913ebe690142b836ed42b72429ab7dc16b055
Jesse Kade <jessekade@Sharp-Machine.local>
2012-12-04 13:38:22 -0800

    all set up

--- wp-content/themes/compass/functions/customize-admin.php
+++ wp-content/themes/compass/functions/customize-admin.php
@@ -33,11 +33,11 @@ function custom_login_logo() {
   echo '<style type="text/css">
     h1 a 
     {
-    	 background-image:url('.get_bloginfo('template_directory').'/img/logo.png) !important;
-		 background-size: 200px 200px !important;
-    	 width: 200px !important; /* Same width as logo */
-    	 height: 200px !important; /* Same height as logo */
-	     margin-left: -20px !important; /* adjust to center logo above login box */
+    	 background-image:url('.get_bloginfo('template_directory').'/img/login-logo.png) !important;
+		 background-size: 320px 129px !important;
+    	 width: 320px !important; /* Same width as logo */
+    	 height: 129px !important; /* Same height as logo */
+	     margin-left: 10px !important; /* adjust to center logo above login box */
 	 }
     </style>';
 }

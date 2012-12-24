1e47ec678ffda9243d07e77197c29c7efd6053f8
Jesse Kade <jessekade@Sharp-Machine.local>
2012-12-24 13:42:15 -0800

    finished blog section

--- wp-content/themes/compass/functions.php
+++ wp-content/themes/compass/functions.php
@@ -479,3 +479,8 @@ if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails' );
         set_post_thumbnail_size( 530, 260, true );
 }
+
+// add_theme_support( 'infinite-scroll', array(
+// 	'container'  => 'content',
+// 	'footer'     => 'page',
+// ) );
--- wp-content/themes/compass/searchform.php
+++ wp-content/themes/compass/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
-        <input type="text" value="" name="s" id="s" placeholder="search" />
-        <input type="submit" id="searchsubmit" value="Search" />
+        <input type="text" value="" name="s" id="s" placeholder="Search" />
+        <input type="submit" id="searchsubmit" value="" />
     </div>
 </form>

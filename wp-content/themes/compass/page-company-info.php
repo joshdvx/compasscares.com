f6b5510388a5c7a2016498bfd1aba2278e085374
Jesse Kade <jessekade@Sharp-Machine.local>
2012-12-26 17:00:31 -0800

    working on company info page

--- wp-content/themes/compass/page-company-info.php
+++ wp-content/themes/compass/page-company-info.php
@@ -4,26 +4,28 @@
 
 	<?php get_template_part( 'loop', 'page' ); ?>
 	
-	<?php if(get_field('compass_leadership')): ?>
-		<?php while (has_sub_field('compass_leadership')): ?>
-		
-			<div class="leader-photo roundy">
-				<img src="<?php the_sub_field('leadership_headshot') ?>" width="155" height="155"><br>
-			</div>
-			
-			<?php the_sub_field('leadership_name') ?><br>
-			<?php the_sub_field('leader_position') ?><br>
-			<div class="bio-intro">
-				<?php the_sub_field('leadership_bio_1') ?>
-			</div>
-			<div class="full-bio">
-				<?php the_sub_field('leadership_bio_2') ?>
-				<button>Read More</button>
-			</div>
-			
-		<?php endwhile; ?>
-	<?php endif; ?>
+	<div id="leadership">
 	
+		<?php if(get_field('compass_leadership')): ?>
+			<?php while (has_sub_field('compass_leadership')): ?>
+			
+				<div class="leader-photo roundy">
+					<img src="<?php the_sub_field('leadership_headshot') ?>" width="155" height="155"><br>
+				</div>
+				
+				<?php the_sub_field('leadership_name') ?><br>
+				<?php the_sub_field('leader_position') ?><br>
+				<div class="bio-intro">
+					<?php the_sub_field('leadership_bio_1') ?>
+				</div>
+				<div class="full-bio">
+					<?php the_sub_field('leadership_bio_2') ?>
+					<button>Read More</button>
+				</div>
+				
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div><!-- #leadership -->	
 </section><!-- #page -->
 
 <?php get_footer(); ?>

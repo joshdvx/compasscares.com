278efe7ba4af43e60f75c55d33276a4379a424bd
Jesse Kade <jessekade@Sharp-Machine.local>
2012-12-27 12:01:40 -0800

    finished careers and opps page

--- wp-content/themes/compass/page-careers-opportunities.php
+++ wp-content/themes/compass/page-careers-opportunities.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
 
-<section id="page">
-	<div class="row">
-		<div class="span12">
-			<?php get_template_part( 'loop', 'page' ); ?>
-		</div>
-	</div>
+<section id="page" class="careers-opps">
+	<?php get_template_part( 'loop', 'page' ); ?>
 		
-	<div class="span12" id="staff-stories">
-		<h2>Career Success Stories</h2>
+	<div class="clearfix" id="staff-stories">
+		
 		<div id="staff-slider">
 			<div class="scroll">
 				<div class="scrollContainer">
 					<?php if(get_field('career_success_stories')): ?>
 						<?php while (has_sub_field('career_success_stories')): ?>
 							<div class="panel" id="<?php the_sub_field('career_story_first-name') ?>">
-								<img src="<?php the_sub_field('career_story_headshot') ?>" alt="<?php the_sub_field('career_story_name') ?>" width="100" height="100"><br>
-								<?php the_sub_field('career_story_first_name') ?> <?php the_sub_field('career_story_last_name') ?><br>
-								<?php the_sub_field('career_success_story') ?><br>
+								<div class="staff-headshot roundy">
+									<img src="<?php the_sub_field('career_story_headshot') ?>" alt="<?php the_sub_field('career_story_name') ?>" width="265" height="265">
+								</div>
+								<div class="staff-info">
+									<h2>Career Success Stories</h2>
+									<div class="staff-name"> <?php the_sub_field('career_story_first_name') ?> <?php the_sub_field('career_story_last_name') ?>
+									</div><!-- .staff-name -->
+									<?php the_sub_field('career_success_story') ?>
+								</div><!-- .staff-info -->
 							</div>
 						<?php endwhile; ?>
 					<?php endif; ?>
@@ -26,19 +28,30 @@
 			<ul class="navigation">
 				<?php if(get_field('career_success_stories')): ?>
 					<?php while (has_sub_field('career_success_stories')): ?>
-						<li><a href="#<?php the_sub_field('career_story_first_name') ?>"><img src="<?php the_sub_field('career_story_headshot') ?>" alt="<?php the_sub_field('career_story_first_name') ?>" width="50" height="50"><br><?php the_sub_field('career_story_first_name') ?></a></li>
+						<li class="roundy">
+							<a href="#<?php the_sub_field('career_story_first_name') ?>">
+								<img src="<?php the_sub_field('career_story_headshot') ?>" alt="<?php the_sub_field('career_story_first_name') ?>" width="110" height="110"><br>
+								<?php the_sub_field('career_story_first_name') ?>
+							</a>
+						</li>
 					<?php endwhile; ?>
 				<?php endif; ?>
 			</ul>
 		</div>
 	</div><!-- .#staff-stories -->
 	
-	<h1>Training</h1>
-	<?php if(get_field('training_resources')): ?>
-		<?php while (has_sub_field('training_resources')): ?>
-			<a href="<?php the_sub_field('training_file') ?>" title="<?php the_sub_field('training_title') ?>"><?php the_sub_field('training_title') ?></a><br>
-		<?php endwhile; ?>
-	<?php endif; ?>
+	<div class="training">
+		<h1>Training</h1>
+		<div class="training-manuals">
+			<?php if(get_field('training_resources')): ?>
+				<?php while (has_sub_field('training_resources')): ?>
+					<a href="<?php the_sub_field('training_file') ?>" title="<?php the_sub_field('training_title') ?>"><?php the_sub_field('training_title') ?></a>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div><!-- .training-manuals -->
+		<a href="<?php bloginfo('url'); ?>/jobs" class="btn pull-right">Go to Job Listings</a>
+	</div><!-- .training -->
+	
 </section><!-- #page -->
 
 <?php get_footer(); ?>
